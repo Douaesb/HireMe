8d48c5542d62f107e5f242e5ec4a332f16a2d903
Douae sb <douae.sb411@example.com>
2024-01-30 12:32:57 +0100

    create service front

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\ServiceController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,12 +22,18 @@ Route::get('/', function () {
 
 
 Route::get('/categorie', [CategorieController::class, 'index'])->name('categorie.index');
-
 Route::get('/categorie/create',[CategorieController::class , 'create'])->name('categorie.create');
 Route::post('/categorie',[CategorieController::class , 'store'])->name('categorie.store');
 Route::get('/categorie/{categorie}/edit',[CategorieController::class , 'edit'])->name('categorie.edit');
 Route::put('/categorie/{categorie}/update',[CategorieController::class , 'update'])->name('categorie.update');
 Route::delete('/categorie/{categorie}/delete',[CategorieController::class , 'delete'])->name('categorie.delete');
+
+Route::get('/service', [ServiceController::class, 'index'])->name('service.index');
+Route::get('/service/create',[ServiceController::class , 'create'])->name('service.create');
+Route::post('/service',[ServiceController::class , 'store'])->name('service.store');
+// Route::get('/service/{service}/edit',[ServiceController::class , 'edit'])->name('service.edit');
+// Route::put('/service/{service}/update',[ServiceController::class , 'update'])->name('service.update');
+// Route::delete('/service/{service}/delete',[ServiceController::class , 'delete'])->name('service.delete');
 
 
 
