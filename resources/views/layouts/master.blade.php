12eddb855b983aab689adc8af44171139b5d6049
Douae sb <douae.sb411@example.com>
2024-01-30 11:34:10 +0100

    home page front

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
+
 <body>
     @include('partials.nav')
     <main>
-
+        @yield('categorie')
+        @yield('home')
         @yield('create')
         @yield('edit')
-        @yield('categorie')
     </main>
     @include('partials.footer')
-
+    <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
 </body>
+
 </html>
